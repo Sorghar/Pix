3a0baae622803ba9f8e3f30f382c8d244c30f4ea
Ing. Marek Lakoštík <lakostik@winkros.local>
2019-09-24 09:20:26 +0200

    Album title above photos

--- src/app/album/album-list/album-list.component.ts
+++ src/app/album/album-list/album-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Album } from 'src/app/core/models/album';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { AlbumState } from '../state/state';
 import { Store, select } from '@ngrx/store';
 import { LoadAlbums } from '../state/actions';
@@ -14,11 +14,17 @@ import { getAllAlbums } from '../state/selectors';
 export class AlbumListComponent implements OnInit {
 
   albums$: Observable<Album[]>;
+  currentAlbumSubject = new Subject<Album>();
+  currentAlbum$ = this.currentAlbumSubject.asObservable();
 
   constructor(private store: Store<AlbumState>) { }
 
   ngOnInit() {
     this.albums$ = this.store.pipe(select(getAllAlbums));
     this.store.dispatch(new LoadAlbums());
+  }
+
+  selectAlbum(album: Album) {
+    this.currentAlbumSubject.next(album);
   }
 }

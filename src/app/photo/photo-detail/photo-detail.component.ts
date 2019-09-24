83d9d8a88314e62006197295dc721a15ce82698d
Martin Mikula <martin.mikula@kros.sk>
2019-09-24 15:15:39 +0200

    photo arrow navigation

--- src/app/photo/photo-detail/photo-detail.component.ts
+++ src/app/photo/photo-detail/photo-detail.component.ts
@@ -3,9 +3,11 @@ import { Store, select } from '@ngrx/store';
 import { PhotoState } from '../state/state';
 import { Observable, Subscription } from 'rxjs';
 import { Photo } from 'src/app/core/models/photo';
-import { getCurrentPhoto } from '../state/selectors';
+import { getCurrentPhoto, getCurrentAlbumPhotos } from '../state/selectors';
 import { ActivatedRoute, Router, NavigationEnd, Event } from '@angular/router';
 import { SetCurrentPhotoId } from '../state/actions';
+import { map } from 'rxjs/operators';
+import { getCurrentAlbum } from '../../album/state/selectors';
 
 @Component({
   selector: 'app-photo-detail',
@@ -16,15 +18,18 @@ export class PhotoDetailComponent implements OnInit, OnDestroy {
 
   currentPhoto$: Observable<Photo>;
   sub: Subscription;
+  photoSub: Subscription;
+  currentAlbumId: number;
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
     private store: Store<PhotoState>
-    ) { }
+  ) { }
 
   ngOnInit() {
     this.currentPhoto$ = this.store.pipe(select(getCurrentPhoto));
+    this.store.select(getCurrentAlbum).subscribe(x => this.currentAlbumId = x ? x.id : 0);
     this.dispatchAction();
     this.sub = this.router.events.subscribe((e: Event) => {
       if (e instanceof NavigationEnd) {
@@ -40,5 +45,20 @@ export class PhotoDetailComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.sub.unsubscribe();
+    this.photoSub.unsubscribe();
   }
+
+  showPhoto(goNext: boolean) {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.photoSub = this.store.pipe(
+      select(getCurrentAlbumPhotos),
+      map(photos => {
+        const photo = photos.find(p => p.id === id);
+        const nextPhoto = photos.indexOf(photo) + (goNext ? 1 : -1);
+        return photos[nextPhoto] ? photos[nextPhoto] : photo;
+      }),
+    ).subscribe(x => this.router.navigate(['albums', this.currentAlbumId, 'photos', x.id]));
+  }
+
+
 }
